74df9be9e5d9471f5e88282d06ecc3fb58bbb1f0
Adriano Cangiamila <adrianocangiamila@gmail.com>
2022-01-30 14:58:31 +0000

    add msg if field is missing

--- server/middlewares/validate.middlewares.js
+++ server/middlewares/validate.middlewares.js
@@ -4,9 +4,9 @@ const validate = (method) => {
     switch (method) {
         case "create_personnel":
             return [
-                body("first_name", "Invalid first name").exists().bail().isAlpha(),
-                body("last_name", "Invalid last name").exists().bail().isAlpha(),
-                body("email", "Invalid email").exists().bail().isEmail(),
+                body("first_name", "Invalid first name").exists().withMessage("Email is required").bail().isAlpha(),
+                body("last_name", "Invalid last name").exists().withMessage("Email is required").bail().isAlpha(),
+                body("email", "Invalid email").exists().withMessage("Email is required").bail().isEmail(),
                 // body("department_id", "Invalid department ID").exists().bail().isInt(),
                 // body("location_id", "Invalid location ID").exists().bail().isInt(),
             ]

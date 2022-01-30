c03c827ce3738a72ddab4f8205f4ce20dc1198ee
Adriano Cangiamila <adrianocangiamila@gmail.com>
2022-01-30 17:21:55 +0000

    POST personnel body sanitize to lower case

--- server/middlewares/validate.middlewares.js
+++ server/middlewares/validate.middlewares.js
@@ -6,9 +6,9 @@ const validate = (method) => {
     switch (method) {
         case "create_personnel":
             return [
-                body("first_name", "Invalid first name").exists().withMessage("Email is required").bail().trim().isAlpha(),
-                body("last_name", "Invalid last name").exists().withMessage("Email is required").bail().trim().isAlpha(),
-                body("email", "Invalid email").exists().withMessage("Email is required").bail().trim().isEmail().normalizeEmail(),
+                body("first_name", "Invalid first name").exists().withMessage("Email is required").bail().trim().toLowerCase().isAlpha(),
+                body("last_name", "Invalid last name").exists().withMessage("Email is required").bail().trim().toLowerCase().isAlpha(),
+                body("email", "Invalid email").exists().withMessage("Email is required").bail().trim().toLowerCase().isEmail().normalizeEmail(),
                 // body("department_id", "Invalid department ID").exists().bail().isInt(),
                 // body("location_id", "Invalid location ID").exists().bail().isInt(),
             ]

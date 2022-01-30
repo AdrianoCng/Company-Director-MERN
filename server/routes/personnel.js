18656f44729e0932b27fa1dacc0a64160eb85843
Adriano Cangiamila <adrianocangiamila@gmail.com>
2022-01-30 11:29:48 +0000

    refactor personnel

--- server/routes/personnel.js
+++ server/routes/personnel.js
@@ -1,8 +1,7 @@
-const express = require("express");
 const validate = require("../middlewares/validate");
 const personnel_controller = require("../controllers/personnel_controller");
 
-const router = express.Router();
+const router = require("express").Router();
 
 // Get all personnel
 router.get("/", personnel_controller.get_all_personnel);

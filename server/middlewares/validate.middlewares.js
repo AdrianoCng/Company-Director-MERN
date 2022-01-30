b5758d683cf9aec289eeb7376d5d59dbcf5573fb
Adriano Cangiamila <adrianocangiamila@gmail.com>
2022-01-30 15:20:09 +0000

    sanitize create personnel body request

--- server/middlewares/validate.middlewares.js
+++ server/middlewares/validate.middlewares.js
@@ -4,9 +4,9 @@ const validate = (method) => {
     switch (method) {
         case "create_personnel":
             return [
-                body("first_name", "Invalid first name").exists().withMessage("Email is required").bail().isAlpha(),
-                body("last_name", "Invalid last name").exists().withMessage("Email is required").bail().isAlpha(),
-                body("email", "Invalid email").exists().withMessage("Email is required").bail().isEmail(),
+                body("first_name", "Invalid first name").exists().withMessage("Email is required").bail().trim().isAlpha(),
+                body("last_name", "Invalid last name").exists().withMessage("Email is required").bail().trim().isAlpha(),
+                body("email", "Invalid email").exists().withMessage("Email is required").bail().trim().isEmail().normalizeEmail(),
                 // body("department_id", "Invalid department ID").exists().bail().isInt(),
                 // body("location_id", "Invalid location ID").exists().bail().isInt(),
             ]

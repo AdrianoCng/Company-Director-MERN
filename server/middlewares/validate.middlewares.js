388037c8f37192b8c7271f605578b9e01b33f44f
Adriano Cangiamila <adrianocangiamila@gmail.com>
2022-02-13 20:10:35 +0000

    refactor validation for POST /api/location

--- server/middlewares/validate.middlewares.js
+++ server/middlewares/validate.middlewares.js
@@ -47,7 +47,7 @@ module.exports = {
     },
     create_location: () => {
         return [
-            body("name").exists().withMessage("Name is required").bail().trim().toLowerCase().isAlpha()
+            body("name").exists().withMessage("Name is required").bail().trim().toLowerCase().isString().escape()
         ]
     },
     update_location: () => {

378b8103a0734c41409ca1170cb5ec6eedc5098c
Adriano Cangiamila <adrianocangiamila@gmail.com>
2022-02-19 15:20:46 +0000

    Router Setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import Sidebar from "./components/sidebar";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import styled from "styled-components";
 
 const App = (): JSX.Element => {
     return (
-        <>
-            <Sidebar />
-        </>
+        <BrowserRouter>
+            <Wrapper>
+                <Sidebar />
+                <Routes>
+                    <Route path="/" element={"Component"} />
+                </Routes>
+            </Wrapper>
+        </BrowserRouter>
     );
 };
+
+const Wrapper = styled.div`
+    margin-left: 290px;
+    padding: 20px;
+    height: 100vh;
+`;
 
 export default App;

22d90ebf557b5bb869319c2e27888124e5d60fec
Adriano Cangiamila <adrianocangiamila@gmail.com>
2022-02-19 14:41:24 +0000

    move global styles in separate file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,9 @@
 import React from "react";
-import { createGlobalStyle } from "styled-components";
 import Sidebar from "./components/sidebar";
-
-const GlobalStyles = createGlobalStyle`
-    *,
-    *::before, 
-    *::after {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-
-    body {
-        font-family: 'Roboto', sans-serif;
-    }
-`;
 
 const App = (): JSX.Element => {
     return (
         <>
-            <GlobalStyles />
             <Sidebar />
         </>
     );
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { QueryClientProvider, QueryClient } from "react-query";
+import GlobalStyles from "./globalStyles";
 
 const queryClient = new QueryClient();
 
 ReactDOM.render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
+            <GlobalStyles />
             <App />
         </QueryClientProvider>
     </React.StrictMode>,

9fc00852a6810bed0ca722db1eb2ce1e176985ef
Adriano Cangiamila <adrianocangiamila@gmail.com>
2022-02-19 11:45:07 +0000

    install styled-components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,11 @@
 import React from "react";
-import { useQuery } from "react-query";
-import { api } from "./utils";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
-    const { data, isError, isLoading } = useQuery("personnel", async () => {
-        const res = await api.get("http://localhost:5000/api/personnel");
-        return res;
-    });
-
     return (
-        <div>
-            {isError && <p>Error fetching data</p>}
-            {isLoading && <p>Loading...</p>}
-            {data?.data.data.map((el: { first_name: string }) => el.first_name)}
-        </div>
+        <>
+            <Sidebar />
+        </>
     );
 };
 

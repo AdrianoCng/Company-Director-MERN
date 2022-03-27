4340e4ed367208e0b31a94c264bc25734d8523d5
MAC/Adriano Cangiamila <adrianocangiamila@gmail.com>
2022-03-27 12:02:24 +0100

    cursor pointer for select field input

--- client/src/components/selectField/styles.tsx
+++ client/src/components/selectField/styles.tsx
@@ -4,6 +4,7 @@ import { inputStyles } from "../../globalStyles";
 export const SelectInput = styled.select`
     ${inputStyles}
     text-transform: capitalize;
+    cursor: pointer;
 `;
 
 export const Label = styled.label`

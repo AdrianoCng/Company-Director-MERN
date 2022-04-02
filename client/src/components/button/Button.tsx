ecb99dc9a22540ecfee61eecc7dec6aa6554afbc
MAC/Adriano Cangiamila <adrianocangiamila@gmail.com>
2022-04-02 21:53:40 +0100

    default type for button

--- client/src/components/button/Button.tsx
+++ client/src/components/button/Button.tsx
@@ -4,7 +4,7 @@ import * as S from "./styles";
 interface Props {
     type?: "button" | "submit" | "reset";
 }
-const Button: React.FC<Props> = ({ children, type }) => {
+const Button: React.FC<Props> = ({ children, type = "button" }) => {
     return <S.Button type={type}>{children}</S.Button>;
 };
 

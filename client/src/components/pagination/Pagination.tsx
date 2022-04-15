aef89050503a43320912f8b7328a8d7dc48a59a1
MAC/Adriano Cangiamila <adrianocangiamila@gmail.com>
2022-04-15 10:46:09 +0100

    change file extensions and create pagination folder and files

---
+++ client/src/components/pagination/Pagination.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./styles";
+
+const Pagination = () => {
+    return <S.PaginationWrapper>Pagination</S.PaginationWrapper>;
+};
+
+export default Pagination;

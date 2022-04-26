5ad533d19deaabb65b85468b753ba4a6e2b23d0a
Adriano Cangiamila <adrianocangiamila@gmail.com>
2022-04-26 12:09:05 +0100

    modal default styles

--- client/src/components/modal/Modal.tsx
+++ client/src/components/modal/Modal.tsx
@@ -1,10 +1,27 @@
-import ReactModal from "react-modal";
+import ReactModal, { Props, Styles } from "react-modal";
 
-interface Props {
-    isOpen: boolean;
-}
-const Modal = ({ isOpen }: Props) => {
-    return <ReactModal isOpen={isOpen}>Hello</ReactModal>;
+const Modal = (props: Props) => {
+    const defaultStyles: Styles = {
+        overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0.4)",
+            ...props.style?.overlay,
+        },
+        content: {
+            height: "600px",
+            width: "800px",
+            left: "50%",
+            top: "50%",
+            transform: "translate(-50%, -50%)",
+            borderRadius: "5px",
+            ...props.style?.content,
+        },
+    };
+
+    return (
+        <ReactModal {...props} style={defaultStyles}>
+            {props.children}
+        </ReactModal>
+    );
 };
 
 export default Modal;

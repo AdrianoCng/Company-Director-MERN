47a580e687d77aec2dcd7bcfdbefe294ab6f3a18
Adriano Cangiamila <adrianocangiamila@gmail.com>
2022-04-30 23:18:18 +0100

    remove unused file

--- client/src/hooks/shared/useReactQueryClient.ts
+++
@@ -1,9 +0,0 @@
-import { useQueryClient, QueryKey } from "react-query";
-
-const useReactQueryClient = <T extends any>(key: QueryKey) => {
-    const queryClient = useQueryClient();
-
-    return queryClient.getQueryData(key);
-};
-
-export default useReactQueryClient;

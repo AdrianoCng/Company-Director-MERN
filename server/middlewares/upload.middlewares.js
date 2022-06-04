e29dcbe6433381f90e0acbadb6e14c5904405883
MAC/Adriano Cangiamila <adrianocangiamila@gmail.com>
2022-06-04 12:24:59 +0100

    remove date string from file name when uploading to s3

--- server/middlewares/upload.middlewares.js
+++ server/middlewares/upload.middlewares.js
@@ -16,9 +16,7 @@ module.exports = [
         if (req.file) {
             const params = {
                 Bucket: AWS_BUCKET_NAME,
-                Key: `${first_name.toLowerCase()}_${last_name.toLowerCase()}/${new Date().toISOString()}-${
-                    req.file.originalname
-                }`,
+                Key: `${first_name.toLowerCase()}_${last_name.toLowerCase()}/${req.file.originalname}`,
                 Body: req.file.buffer,
                 ACL: "public-read",
                 ContentType: "image/jpeg",
